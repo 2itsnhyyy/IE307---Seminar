c2a0a77951acded163276bb6052c4b777683b285
anhthotn <22521424@gm.uit.edu.vn>
2024-11-17 17:03:20 +0700

    update
    add dropdown

--- components/Dropdown/MultiSelectDropdown.js
+++ components/Dropdown/MultiSelectDropdown.js
@@ -1,12 +1,183 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
+import MultiSelect from 'react-native-multiple-select';
+
+// Dữ liệu bài đăng mẫu
+const posts = [
+  {
+    id: 0,
+    avatar: require("./images/ava.png"),
+    username: "PuwuNek",
+    status: "Hi, hope you have a great day!",
+    posting: require("./images/post.png"),
+    state: true,
+    like: 100,
+    share: 30,
+    comment: 450,
+  },
+  {
+    id: 1,
+    avatar: require("./images/ava2.png"),
+    username: "Reinek",
+    status: "What a beautiful day!",
+    posting: require("./images/post2.png"),
+    state: true,
+    like: 200,
+    share: 50,
+    comment: 60,
+  },
+  {
+    id: 2,
+    avatar: require("./images/ava3.png"),
+    username: "Ningningnek",
+    status: "My first potshot!",
+    posting: require("./images/post3.png"),
+    state: true,
+    like: 80,
+    share: 99,
+    comment: 190,
+  },
+  {
+    id: 3,
+    avatar: require("./images/ava4.png"),
+    username: "ItsmeYujin",
+    status: "My first potshot!",
+    posting: require("./images/post4.png"),
+    state: true,
+    like: 80,
+    share: 99,
+    comment: 190,
+  },
+  {
+    id: 4,
+    avatar: require("./images/ava5.png"),
+    username: "SanaNek",
+    status: "My first potshot!",
+    posting: require("./images/post5.png"),
+    state: true,
+    like: 80,
+    share: 99,
+    comment: 190,
+  },
+  {
+    id: 5,
+    avatar: require("./images/ava7.png"),
+    username: "ImNayeon",
+    status: "My first potshot!",
+    posting: require("./images/post7.png"),
+    state: true,
+    like: 80,
+    share: 99,
+    comment: 190,
+  },
+];
 
 const MultiSelectDropdown = () => {
-  return (
-    <View>
-      <Text>MultiSelectDropdown</Text>
-    </View>
-  )
-}
+  const [selectedPosts, setSelectedPosts] = useState([]);
+  const [showSelected, setShowSelected] = useState(false);
 
-export default MultiSelectDropdown
+  const onSelectedItemsChange = (selectedPosts) => {
+    setSelectedPosts(selectedPosts);
+    setShowSelected(selectedPosts.length > 0);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+
+        <Text style={styles.titleText}>
+          Ví dụ về Multiple Select / Dropdown / Picker Example in React Native
+        </Text>
+
+        <MultiSelect
+          hideTags={false}
+          items={posts}
+          uniqueKey="id"
+          onSelectedItemsChange={onSelectedItemsChange}
+          selectedItems={selectedPosts}
+          selectText="Pick Posts"
+          searchInputPlaceholderText="Search Posts..."
+          onChangeInput={(text) => console.log(text)}
+          tagRemoveIconColor="#CCC"
+          tagBorderColor="#CCC"
+          tagTextColor="#CCC"
+          selectedItemTextColor="#CCC"
+          selectedItemIconColor="#CCC"
+          itemTextColor="#000"
+          displayKey="username"
+          searchInputStyle={{color: '#CCC'}}
+          submitButtonColor="deepskyblue"
+          submitButtonText="Submit"
+        />
+
+        <ScrollView>
+          <Text style={styles.headingText}>Posts:</Text>
+          {(showSelected ? selectedPosts : posts.map(post => post.id)).map((postId) => {
+            const post = posts.find(p => p.id === postId);
+            return (
+              <View key={post.id}>
+                <View style={styles.ava}>
+                  <Image style={styles.avatar} source={post.avatar} />
+                  <Text style={styles.name}>{post.username}</Text>
+                </View>
+                <Text style={styles.status}>{post.status}</Text>
+                <Image style={styles.stat} source={post.posting} />
+              </View>
+            );
+          })}
+        </ScrollView>
+
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  titleText: {
+    padding: 8,
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  headingText: {
+    padding: 8,
+  },
+  ava: {
+    flexDirection: "row",
+    paddingTop: 20,
+    marginLeft: 20,
+    paddingBottom: 15,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 30,
+    overflow: "hidden",
+  },
+  name: {
+    padding: 5,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  status: {
+    paddingLeft: 20,
+    paddingBottom: 10,
+    fontSize: 20,
+    color: "#000",
+  },
+  stat: {
+    width: 385,
+    height: 300,
+    marginLeft: 15,
+    marginRight: 15,
+    borderRadius: 10,
+  },
+});
+
+export default MultiSelectDropdown;
--- components/Dropdown/SearchableDropdown.js
+++ components/Dropdown/SearchableDropdown.js
@@ -1,12 +1,243 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+} from "react-native";
+import SearchableDropdown from "react-native-searchable-dropdown";
+import { useNavigation } from "@react-navigation/native";
+import { ScrollView } from "react-native-gesture-handler";
 
-const SearchableDropdown = () => {
+const items = [
+  { id: 1, name: "nhom1" },
+  { id: 2, name: "nhom2" },
+  { id: 3, name: "nhom3" },
+  { id: 4, name: "thiennhi" },
+  { id: 5, name: "phuongquynh" },
+  { id: 6, name: "anhtho" },
+  { id: 7, name: "laptrinh" },
+  { id: 8, name: "congnghe" },
+  { id: 9, name: "Web" },
+  { id: 10, name: "App" },
+];
+
+const SearchableDropdownEx = () => {
+  const navigation = useNavigation();
+  const [selectedItems1, setSelectedItems1] = useState([]);
+  const [selectedItems2, setSelectedItems2] = useState([]);
+
+  const selectedItemsChange1 = (selectedItems1) => {
+    setSelectedItems1(selectedItems1);
+  };
+  const selectedItemsChange2 = (selectedItems2) => {
+    setSelectedItems2(selectedItems2);
+  };
+  const removeItem1 = (item) => {
+    const items = selectedItems1.filter((sitem) => sitem.id !== item.id);
+    setSelectedItems1(items);
+  };
+  const removeItem2 = (item) => {
+    const items = selectedItems2.filter((sitem) => sitem.id !== item.id);
+    setSelectedItems2(items);
+  };
+
+  // Lấy nguồn data cho phần tử dropdown từ server
+  const [serverData, setServerData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://192.168.1.56:3000/data");
+        const data = await response.json();
+        // Kiểm tra dữ liệu
+        console.log("Fetched data:", data);
+        // Cập nhật dữ liệu từ server
+        setServerData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <View>
-      <Text>SearchableDropdown</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.titleText}>
+          Ví dụ về Searchable Dropdown / Picker trong React Native
+        </Text>
 
-export default SearchableDropdown
+        <Text style={styles.headingText}>Searchable Dropdown từ mảng tĩnh</Text>
+
+        <SearchableDropdown
+          //Theo dõi hoặc ghi nhận giá trị mà người dùng nhập vào ô tìm kiếm
+          onTextChange={(text) => console.log(text)}
+          // Được gọi sau khi lựa chọn một mục từ dropdown
+          onItemSelect={(item) =>
+            selectedItemsChange1([...selectedItems1, item])
+          }
+          // Được gọi để xóa một mục khỏi danh sách đã chọn
+          onRemoveItem={(item, index) => {
+            const items = selectedItems1.filter(
+              (sitem) => sitem.id !== item.id
+            );
+            setSelectedItems1(items);
+            console.log("Item removed:", item);
+          }}
+          // Thay đổi kiểu dáng của container
+          containerStyle={{ padding: 5 }}
+          // Thay đổi kiểu dáng của ô nhập dữ liệu tìm kiếm
+          textInputStyle={styles.textInputStyle}
+          // Thay đổi kiểu dáng của một mục đơn để select trong dropdown
+          itemStyle={styles.itemStyle}
+          // Thay đổi kiểu dáng của văn bản trong một mục đơn
+          itemTextStyle={styles.itemTextStyle}
+          // Thay đổi kiểu dáng của container chứa các mục
+          itemsContainerStyle={styles.itemContainerStyle}
+          // Mapping của mảng các mục
+          items={items} // Mảng các mục
+          placeholder="placeholder" // Nôi dung hiển thị ban đầu cho ô tìm kiếm
+          resetValue={false} // Chọn false để giữ giá trị sau khi chọn và true để xóa giá trị sau khi chọn-chọn true sẽ cho bạn chọn liên tục
+          underlineColorAndroid="transparent" // Thay đổi style gạch chân cho ô tìm kiếm trên Android
+        />
+
+        <ScrollView>
+          <Text style={styles.headingText}>Các mục đã được chọn:</Text>
+          {selectedItems1.map((item, index) => (
+            <View key={index} style={styles.selectedItem}>
+              <Text>{item.name}</Text>
+              <TouchableOpacity onPress={() => removeItem1(item)}>
+                <Text style={styles.removeButton}>Xóa</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
+        <Text style={styles.headingText}>Searchable Dropdown từ Server</Text>
+
+        <SearchableDropdown
+          //Theo dõi hoặc ghi nhận giá trị mà người dùng nhập vào ô tìm kiếm
+          onTextChange={(text) => console.log(text)}
+          // Được gọi sau khi lựa chọn một mục từ dropdown
+          onItemSelect={(item) =>
+            selectedItemsChange2([...selectedItems2, item])
+          }
+          // Được gọi để xóa một mục khỏi danh sách đã chọn
+          onRemoveItem={(item, index) => {
+            const items = selectedItems2.filter(
+              (sitem) => sitem.id !== item.id
+            );
+            setSelectedItems2(items);
+            console.log("Item removed:", item);
+          }}
+          // Thay đổi kiểu dáng của container
+          containerStyle={{ padding: 5 }}
+          // Thay đổi kiểu dáng của ô nhập dữ liệu tìm kiếm
+          textInputStyle={styles.textInputStyle}
+          // Thay đổi kiểu dáng của một mục đơn để select trong dropdown
+          itemStyle={styles.itemStyle}
+          // Thay đổi kiểu dáng của văn bản trong một mục đơn
+          itemTextStyle={styles.itemTextStyle}
+          // Thay đổi kiểu dáng của container chứa các mục
+          itemsContainerStyle={styles.itemContainerStyle}
+          // Mapping của mảng các mục
+          items={serverData} // Mảng các mục
+          placeholder="placeholder" // Nôi dung hiển thị ban đầu cho ô tìm kiếm
+          resetValue={false} // Chọn false để giữ giá trị sau khi chọn và true để xóa giá trị sau khi chọn-chọn true sẽ cho bạn chọn liên tục
+          underlineColorAndroid="transparent" // Thay đổi style gạch chân cho ô tìm kiếm trên Android
+        />
+
+        <ScrollView>
+          <Text style={styles.headingText}>Các mục đã được chọn:</Text>
+          {selectedItems2.map((item, index) => (
+            <View key={index} style={styles.selectedItem}>
+              <Text>{item.name}</Text>
+              <TouchableOpacity onPress={() => removeItem2(item)}>
+                <Text style={styles.removeButton}>Xóa</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
+        </ScrollView>
+
+        <TouchableOpacity
+          style={[styles.button, styles.navButton]}
+          onPress={() => navigation.navigate("MultiSelectDropdown")}
+        >
+          <Text style={styles.buttonText}>Go to MultiSelect Dropdown</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    padding: 10,
+  },
+  titleText: {
+    padding: 8,
+    fontSize: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  headingText: {
+    padding: 8,
+  },
+  selectedItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: 8,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+  },
+  textInputStyle: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    backgroundColor: "#FAF7F6",
+  },
+  itemStyle: {
+    padding: 10,
+    marginTop: 2,
+    backgroundColor: "#FAF9F8",
+    borderColor: "#bbb",
+    borderWidth: 1,
+  },
+  itemTextStyle: {
+    color: "#222",
+  },
+  itemContainerStyle: {
+    maxHeight: "50%",
+  },
+  removeButton: {
+    color: "red",
+  },
+  button: {
+    backgroundColor: "#007BFF",
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 8,
+    marginVertical: 10,
+    alignSelf: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: "center",
+  },
+  navButtonsContainer: {
+    marginTop: 20,
+    width: "100%",
+    alignItems: "center",
+  },
+  navButton: {
+    width: "80%",
+    marginBottom: 15,
+  },
+});
+
+export default SearchableDropdownEx;
